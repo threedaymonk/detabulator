10cc221d07ed49b92c5a85eff01b26326e075720
Paul Battley <pbattley@gmail.com>
2010-07-18 01:29:26 +0100

    Refactored into something that might make sense to others.

--- lib/detabulator.rb
+++ lib/detabulator.rb
@@ -2,32 +2,49 @@ class Detabulator
   SPACE = 32
 
   def detabulate(s)
-    lines = s.split(/\n/).map{ |line| line.unpack("U*") }
+    lines   = s.split(/\n/).map{ |line| line.unpack("U*") }
+    lengths = extract_segment_lengths(
+                collapse_space_clusters(
+                  space_mask(lines)))
+
+    offset  = nil # GC help
+    lines.map{ |line|
+      offset = 0
+      lengths.map{ |length|
+        cell = (line[offset, length] || []).pack('U*').strip
+        offset += length
+        cell }}
+  end
+
+private
+  #    aa   bb
+  #    c    dddd ee
+  # => ..TTT....T..
+  #
+  def space_mask(lines)
     max_line_length = lines.map{ |a| a.length }.max
-    mask = [true] * max_line_length
-    lines.each do |line|
-      line_mask = (0 ... max_line_length).map{ |i| (line[i] || SPACE) == SPACE }
-      mask = mask.zip(line_mask).map{ |a, b| a & b }
-    end
-    (max_line_length - 1).downto(1).each do |i|
-      mask[i] = false if mask[i-1]
-    end
-    segment_lengths = mask.inject([1]){ |a,e|
+    lines.inject([true] * max_line_length){ |mask, line|
+      mask.zip(line).
+           map{ |a, b| a && (!b || b == SPACE) }}
+  end
+
+  #    ..TTT....T..
+  # => ..T......T..
+  #
+  def collapse_space_clusters(mask)
+    rmask = mask.reverse
+    rmask.zip(rmask[1..-1]).map{ |a, b| a && !b }.reverse
+  end
+
+  # ..T......T.. => [3, 7, 2]
+  #
+  def extract_segment_lengths(mask)
+    mask.inject([1]){ |a,e|
       if e
         a << 1
       else
         a[-1] += 1
-      end
-      a
-    }
-    offset = nil
-    lines.map{ |line|
-      offset = 0
-      segment_lengths.map{ |length|
-        word = (line[offset, length] || []).pack('U*').strip
-        offset += length
-        word
-      }
-    }
+        a
+      end }
   end
 end
